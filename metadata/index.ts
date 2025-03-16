99d29b1284f41b543951f4e293579f51242a9bad
小飘 <110318627+mynamexiaopiao@users.noreply.github.com>
2025-03-16 08:43:20 +0800

    优化界面

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,15 +1,15 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'xiaopiao'
+export const siteShortName = 'xiaopiao'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
-export const include = ['笔记', '生活']
+export const include = ['neoforge', '其它']
 
 /** Repo */
 export const githubRepoLink = 'https://github.com/nolebase/nolebase'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = ''
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
